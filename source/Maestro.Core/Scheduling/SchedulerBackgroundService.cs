6e199656ab72a298704a2c85c954410c9bb79653
Eoin Motherway <25342760+YuKitsune@users.noreply.github.com>
2025-07-08 21:40:37 +1000

    chore: Clean up Scheduler

--- source/Maestro.Core/Scheduling/SchedulerBackgroundService.cs
+++ source/Maestro.Core/Scheduling/SchedulerBackgroundService.cs
@@ -22,12 +22,12 @@ public class SchedulerBackgroundService(
     public async Task Start(string airportIdentifier, CancellationToken cancellationToken = default)
     {
         logger.Information("Starting sequence for {AirportIdentifier}", airportIdentifier);
-        
+
         using (await _semaphore.LockAsync(cancellationToken))
         {
             if (_sequences.ContainsKey(airportIdentifier))
                 throw new MaestroException($"Sequence for {airportIdentifier} has already started.");
-            
+
             var taskLogger = logger.ForContext("Sequence", airportIdentifier);
             var cancellationTokenSource = new CancellationTokenSource();
             var task = DoSequence(
@@ -39,29 +39,29 @@ public class SchedulerBackgroundService(
                 clock,
                 taskLogger,
                 cancellationTokenSource.Token);
-            
+
             _sequences.Add(airportIdentifier, (task, cancellationTokenSource));
-            
+
             logger.Debug("Started sequence for {AirportIdentifier}", airportIdentifier);
         }
     }
-    
+
     public async Task Stop(string airportIdentifier, CancellationToken cancellationToken = default)
     {
         logger.Information("Stopping sequence for {AirportIdentifier}", airportIdentifier);
-        
+
         using (await _semaphore.LockAsync(cancellationToken))
         {
             if (!_sequences.TryGetValue(airportIdentifier, out var tuple))
                 throw new MaestroException($"Sequence for {airportIdentifier} has not been started.");
-            
+
             var (task, cancellationTokenSource) = tuple;
-            
+
             cancellationTokenSource.Cancel();
             await task;
 
             _sequences.Remove(airportIdentifier);
-            
+
             logger.Debug("Stopped sequence for {AirportIdentifier}", airportIdentifier);
         }
     }
@@ -86,7 +86,7 @@ public class SchedulerBackgroundService(
                     logger.Debug("Lock on {AirportIdentifier} acquired", airportIdentifier);
 
                     if (lockedSequence.Sequence.NextRunwayMode is not null &&
-                        lockedSequence.Sequence.RunwayModeChangeTime <= clock.UtcNow())
+                        lockedSequence.Sequence.RunwayModeChangeTime.IsSameOrBefore(clock.UtcNow()))
                     {
                         var nextRunwayMode = lockedSequence.Sequence.NextRunwayMode;
                         lockedSequence.Sequence.ChangeRunwayMode(nextRunwayMode);
@@ -94,7 +94,7 @@ public class SchedulerBackgroundService(
                             "Runway mode for {AirportIdentifier} changed to {RunwayModeIdentifier}",
                             airportIdentifier,
                             nextRunwayMode.Identifier);
-                    
+
                         await mediator.Publish(
                             new RunwayModeChangedNotification(
                                 airportIdentifier,
@@ -103,16 +103,16 @@ public class SchedulerBackgroundService(
                                 default),
                             cancellationToken);
                     }
-                
+
                     logger.Information("Scheduling {AirportIdentifier}", airportIdentifier);
                     lockedSequence.Sequence.Schedule(scheduler);
                     logger.Debug("Completed scheduling {AirportIdentifier}", airportIdentifier);
-                
+
                     logger.Debug("Cleaning {AirportIdentifier}", airportIdentifier);
                     sequenceCleaner.CleanUpFlights(lockedSequence.Sequence);
                     logger.Debug("Completed cleaning {AirportIdentifier}", airportIdentifier);
                 }
-                
+
                 await Task.Delay(TimeSpan.FromSeconds(30), cancellationToken);
             }
             catch (TaskCanceledException)
