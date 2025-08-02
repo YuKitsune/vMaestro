8907eac49d0fa8a6d9873b2f344430bed6ff8340
Eoin Motherway <25342760+YuKitsune@users.noreply.github.com>
2025-08-02 16:38:42 +1000

    Flow controls

--- source/Maestro.Core/Model/IScheduler.cs
+++ source/Maestro.Core/Model/IScheduler.cs
@@ -14,16 +14,14 @@ public interface IScheduler
 // - When a flight is added and the leader is far away, no delay is applied
 // - When a flight is added and the leader is too close, delay is applied
 // - When a flight is added and the leader is too close, and other runways are available, other runway is assigned
+// - When a flight is added and the leader is too close, all runways result in a delay, the runway with the least delay is assigned
 // - When a flight has NoDelay or ManualLandingTime, it should not be delayed
 // - When a flight has NoDelay or ManualLandingTime, and the leader is too close, the leader is delayed
 // - When a flight has NoDelay or ManualLandingTime, and the leader is too close, and other runways are available, the leader is assigned to the other runway
 // - When a flight has NoDelay or ManualLandingTime, and the leader is too close, and the leader is also NoDelay or ManualLandingTime, no delay is applied
-// - If a flight is delayed until after a runway change, the new runway and landing rate are used
-
-// TODO:
-// A stable aircraft may be pushed back in the sequence by;
-// - an aircraft being manually changed to the same runway,or
-// - a new aircraft entering the sequence with an earlier ETA_FF
+// - When a flight is delayed until after a runway change, the new runway and landing rate are used
+// - When a new flight is added, with ETAs earlier than a stable flight, the stable flight is delayed
+// - When a flight is moved manually, and conflicts with a stable flight, the stable flight is delayed
 
 public class Scheduler(
     IRunwayAssigner runwayAssigner,
@@ -126,6 +124,17 @@ tryAgain:
             {
                 currentRunwayMode = sequence.NextRunwayMode;
                 goto tryAgain;
+            }
+
+            // TODO: Double check how this is supposed to work
+            var performance = performanceLookup.GetPerformanceDataFor(flight.AircraftType);
+            if (performance is not null && performance.AircraftCategory == AircraftCategory.Jet && proposedLandingTime.Value.IsAfter(flight.EstimatedLandingTime))
+            {
+                flight.SetFlowControls(FlowControls.S250);
+            }
+            else
+            {
+                flight.SetFlowControls(FlowControls.ProfileSpeed);
             }
 
             Schedule(flight, proposedLandingTime.Value, proposedRunway);
