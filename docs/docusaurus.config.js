d3d523cbe934c1bbc9be5f44da59749a817e1d7d
Eoin Motherway <25342760+YuKitsune@users.noreply.github.com>
2025-09-14 17:22:47 +1000

    Fix docs link

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -51,8 +51,7 @@ const config = {
           sidebarPath: './sidebars.js',
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
-          editUrl:
-            'https://github.com/yukitsune/vMaestro/tree/main/docs/',
+          editUrl: 'https://github.com/yukitsune/vMaestro/',
         },
         theme: {
           customCss: './src/css/custom.css',
