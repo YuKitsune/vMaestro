62e8db375e631c74d9f2756a61a887f5c1a649fc
Eoin Motherway <25342760+YuKitsune@users.noreply.github.com>
2025-11-10 10:55:18 +1000

    test: Ensure previous master is demoted

--- source/Maestro.Server.Tests/ConnectRequestHandlerTests.cs
+++ source/Maestro.Server.Tests/ConnectRequestHandlerTests.cs
@@ -153,12 +153,14 @@ public class ConnectRequestHandlerTests
         existingConnection.IsMaster.ShouldBeFalse();
         newConnection.IsMaster.ShouldBeTrue();
 
+        // OwnershipRevoked should be sent to the OLD master (existingConnection)
         hubProxy.Verify(x => x.Send(
-            connectionId,
+            existingConnection.Id,
             "OwnershipRevoked",
             It.Is<OwnershipRevokedNotification>(n => n.AirportIdentifier == airportIdentifier),
             It.IsAny<CancellationToken>()), Times.Once);
 
+        // PeerConnected should be sent to the existing connection
         hubProxy.Verify(x => x.Send(
             existingConnection.Id,
             "PeerConnected",
@@ -168,6 +170,7 @@ public class ConnectRequestHandlerTests
                 n.Role == role),
             It.IsAny<CancellationToken>()), Times.Once);
 
+        // ConnectionInitialized with IsMaster=true should be sent to the NEW Flow controller
         hubProxy.Verify(x => x.Send(
             connectionId,
             "ConnectionInitialized",
