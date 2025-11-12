d10d9f7fb999d17755f54ba0d79d6b7d5ffddb4a
Eoin Motherway <25342760+YuKitsune@users.noreply.github.com>
2025-11-12 21:44:14 +1000

    wip

--- source/Maestro.Core/Handlers/MoveFlightRequestHandler.cs
+++ source/Maestro.Core/Handlers/MoveFlightRequestHandler.cs
@@ -32,7 +32,21 @@ public class MoveFlightRequestHandler(
         var sequence = lockedSession.Session.Sequence;
         var flight = sequence.FindFlight(request.Callsign);
         if (flight is null)
-            return;
+            throw new MaestroException($"{request.Callsign} not found");
+
+        var newIndex = sequence.IndexOf(request.NewLandingTime);
+
+        var runwayMode = sequence.GetRunwayModeAt(request.NewLandingTime);
+        var runwayIdentifier = runwayMode.Runways.FirstOrDefault(r => request.RunwayIdentifiers.Contains(r.Identifier))?.Identifier
+            ?? runwayMode.Default.Identifier;
+
+        // TODO: Manually set the runway for now, but we need to revisit this later
+        // Re: delaying into a new runway mode
+        flight.SetRunway(runwayIdentifier, manual: true);
+
+        TODO finish me
+
+
 
         // Delegate to Sequence aggregate to handle the move operation
         sequence.MoveFlight(
