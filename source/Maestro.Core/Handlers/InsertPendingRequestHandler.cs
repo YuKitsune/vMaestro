431b63f29615f477bf4abf2b4092a32fa020e44c
Eoin Motherway <25342760+YuKitsune@users.noreply.github.com>
2025-11-13 10:05:52 +1000

    wip: Insert Pending

--- source/Maestro.Core/Handlers/InsertPendingRequestHandler.cs
+++ source/Maestro.Core/Handlers/InsertPendingRequestHandler.cs
@@ -30,9 +30,7 @@ public class InsertPendingRequestHandler(
         using var lockedSession = await sessionManager.AcquireSession(request.AirportIdentifier, cancellationToken);
         var sequence = lockedSession.Session.Sequence;
 
-        var flight = sequence.PendingFlights.SingleOrDefault(f =>
-            f.Callsign == request.Callsign);
-
+        var flight = lockedSession.Session.PendingFlights.SingleOrDefault(f => f.Callsign == request.Callsign);
         if (flight is null)
         {
             // TODO: Confirm what should happen in this case
@@ -41,17 +39,50 @@ public class InsertPendingRequestHandler(
             throw new MaestroException($"{request.Callsign} was not found in the pending list.");
         }
 
+        int index;
+        Runway runway;
+
         switch (request.Options)
         {
             case ExactInsertionOptions exactInsertionOptions:
-                sequence.InsertPending(flight.Callsign, exactInsertionOptions.TargetLandingTime, exactInsertionOptions.RunwayIdentifiers);
+            {
+                var runwayMode = sequence.GetRunwayModeAt(exactInsertionOptions.TargetLandingTime);
+                runway = runwayMode.Runways.FirstOrDefault(r => exactInsertionOptions.RunwayIdentifiers.Contains(r.Identifier))
+                         ?? runwayMode.Default;
+
+                index = sequence.IndexOf(exactInsertionOptions.TargetLandingTime);
                 break;
+            }
             case RelativeInsertionOptions relativeInsertionOptions:
-                sequence.InsertPending(flight.Callsign, relativeInsertionOptions.Position, relativeInsertionOptions.ReferenceCallsign);
+            {
+                var referenceFlight = sequence.FindFlight(relativeInsertionOptions.ReferenceCallsign);
+                if (referenceFlight is null)
+                    throw new MaestroException($"{relativeInsertionOptions.ReferenceCallsign} not found");
+
+                var referenceFlightIndex = sequence.IndexOf(referenceFlight);
+                index = relativeInsertionOptions.Position switch
+                {
+                    RelativePosition.Before => referenceFlightIndex,
+                    RelativePosition.After => referenceFlightIndex + 1,
+                    _ => throw new ArgumentOutOfRangeException()
+                };
+
+                var runwayMode = sequence.GetRunwayModeAt(referenceFlightIndex);
+                runway = runwayMode.Runways.FirstOrDefault(r => r.Identifier == referenceFlight.AssignedRunwayIdentifier)
+                         ?? runwayMode.Default;
                 break;
+            }
             default:
-                throw new ArgumentOutOfRangeException();
+                throw new NotSupportedException($"Cannot insert flight with {request.Options.GetType().Name}");
         }
+
+        // TODO: Feels odd to do this validation here, but we don't want to mutate the runway if the insertion is invalid
+        // Maybe we _can_ mutate the runway? The code will run again and re-assign the runway anyway if we try again
+        sequence.ThrowIfSlotIsUnavailable(index, runway.Identifier);
+
+        flight.SetRunway(runway.Identifier, manual: true);
+
+        sequence.Insert(index, flight);
 
         flight.SetState(State.Stable, clock);
 
