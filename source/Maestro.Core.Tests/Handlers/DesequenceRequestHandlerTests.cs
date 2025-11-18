a8389be7aace1572237887f48cdb6621a4d1aad1
Eoin Motherway <25342760+YuKitsune@users.noreply.github.com>
2025-11-18 16:10:16 +1000

    tests: Add DesequenceRequestHandlerTests

--- source/Maestro.Core.Tests/Handlers/DesequenceRequestHandlerTests.cs
+++ source/Maestro.Core.Tests/Handlers/DesequenceRequestHandlerTests.cs
@@ -1,55 +1,140 @@
-﻿namespace Maestro.Core.Tests.Handlers;
+using Maestro.Core.Handlers;
+using Maestro.Core.Messages;
+using Maestro.Core.Model;
+using Maestro.Core.Tests.Builders;
+using Maestro.Core.Tests.Fixtures;
+using Maestro.Core.Tests.Mocks;
+using MediatR;
+using NSubstitute;
+using Serilog;
+using Shouldly;
 
-public class DesequenceRequestHandlerTests
+namespace Maestro.Core.Tests.Handlers;
+
+public class DesequenceRequestHandlerTests(AirportConfigurationFixture airportConfigurationFixture, ClockFixture clockFixture)
 {
     [Fact]
     public async Task TheFlightIsRemovedFromTheSequenceAndAddedToTheDesequencedList()
     {
-        await Task.CompletedTask;
-        Assert.Fail("Not implemented");
+        var now = clockFixture.Instance.UtcNow();
 
         // Arrange
-        // TODO: Create a flight and add it to the sequence
+        var flight = new FlightBuilder("QFA1")
+            .WithLandingEstimate(now.AddMinutes(10))
+            .WithLandingTime(now.AddMinutes(10))
+            .WithRunway("34L")
+            .Build();
+
+        var (instanceManager, _, session, sequence) = new InstanceBuilder(airportConfigurationFixture.Instance)
+            .WithSequence(s => s.WithFlightsInOrder(flight))
+            .Build();
+
+        sequence.Flights.Count.ShouldBe(1, "Flight should be in sequence");
+        session.DeSequencedFlights.Count.ShouldBe(0, "No desequenced flights initially");
+
+        var mediator = Substitute.For<IMediator>();
+
+        var handler = new DesequenceRequestHandler(
+            instanceManager,
+            new MockLocalConnectionManager(),
+            mediator,
+            Substitute.For<ILogger>());
+
+        var request = new DesequenceRequest("YSSY", "QFA1");
 
         // Act
-        // TODO: Desequence the flight
+        await handler.Handle(request, CancellationToken.None);
 
         // Assert
-        // TODO: Assert that the flight is removed from the sequence
-        // TODO: Assert that the flight has been added to the DesequenecdFlights list
+        sequence.Flights.Count.ShouldBe(0, "Flight should be removed from sequence");
+        session.DeSequencedFlights.Count.ShouldBe(1, "Flight should be added to desequenced list");
+        session.DeSequencedFlights[0].Callsign.ShouldBe("QFA1", "Desequenced flight should be QFA1");
     }
 
     [Fact]
     public async Task TheSequenceIsRecalculated()
     {
-        await Task.CompletedTask;
-        Assert.Fail("Not implemented");
+        var now = clockFixture.Instance.UtcNow();
 
         // Arrange
-        // TODO: Create three flights and add them to the sequence
+        var flight1 = new FlightBuilder("QFA1")
+            .WithLandingEstimate(now.AddMinutes(10))
+            .WithLandingTime(now.AddMinutes(10))
+            .WithRunway("34L")
+            .Build();
+
+        var flight2 = new FlightBuilder("QFA2")
+            .WithLandingEstimate(now.AddMinutes(10))
+            .WithLandingTime(now.AddMinutes(13))
+            .WithRunway("34L")
+            .Build();
+
+        var flight3 = new FlightBuilder("QFA3")
+            .WithLandingEstimate(now.AddMinutes(10))
+            .WithLandingTime(now.AddMinutes(16))
+            .WithRunway("34L")
+            .Build();
+
+        var (instanceManager, _, _, sequence) = new InstanceBuilder(airportConfigurationFixture.Instance)
+            .WithSequence(s => s.WithFlightsInOrder(flight1, flight2, flight3))
+            .Build();
+
+        var originalFlight1LandingTime = flight1.LandingTime;
+
+        var mediator = Substitute.For<IMediator>();
+
+        var handler = new DesequenceRequestHandler(
+            instanceManager,
+            new MockLocalConnectionManager(),
+            mediator,
+            Substitute.For<ILogger>());
+
+        var request = new DesequenceRequest("YSSY", "QFA2");
 
         // Act
-        // TODO: Desequence the second flight
+        await handler.Handle(request, CancellationToken.None);
 
         // Assert
-        // TODO: Assert that the first flight remains unchanged
-        // TODO: Assert that the third flight's landing time has been recalculated
+        flight1.LandingTime.ShouldBe(originalFlight1LandingTime, "QFA1 should remain unchanged");
+        flight3.LandingTime.ShouldBe(flight1.LandingTime.Add(airportConfigurationFixture.AcceptanceRate), "QFA3 should move forward to take QFA2's place");
     }
 
     [Fact]
     public async Task RedirectedToMaster()
     {
-        await Task.CompletedTask;
-        Assert.Fail("Not implemented");
+        var now = clockFixture.Instance.UtcNow();
 
         // Arrange
-        // TODO: Create a dummy connection that simulates a non-master instance
-        // TODO: Create a flight
+        var flight = new FlightBuilder("QFA1")
+            .WithLandingEstimate(now.AddMinutes(10))
+            .WithLandingTime(now.AddMinutes(10))
+            .WithRunway("34L")
+            .Build();
+
+        var (instanceManager, _, session, sequence) = new InstanceBuilder(airportConfigurationFixture.Instance)
+            .WithSequence(s => s.WithFlightsInOrder(flight))
+            .Build();
+
+        sequence.Flights.Count.ShouldBe(1, "Flight should be in sequence");
+
+        var slaveConnectionManager = new MockSlaveConnectionManager();
+        var mediator = Substitute.For<IMediator>();
+
+        var handler = new DesequenceRequestHandler(
+            instanceManager,
+            slaveConnectionManager,
+            mediator,
+            Substitute.For<ILogger>());
+
+        var request = new DesequenceRequest("YSSY", "QFA1");
 
         // Act
-        // TODO: Desequence the flight
+        await handler.Handle(request, CancellationToken.None);
 
         // Assert
-        // TODO: Assert that the request was redirected to the master and not handled locally
+        slaveConnectionManager.Connection.InvokedRequests.Count.ShouldBe(1, "Request should be relayed to master");
+        slaveConnectionManager.Connection.InvokedRequests[0].ShouldBe(request, "The relayed request should match the original request");
+        sequence.Flights.Count.ShouldBe(1, "Flight should not be removed locally when relaying to master");
+        session.DeSequencedFlights.Count.ShouldBe(0, "Flight should not be desequenced locally when relaying to master");
     }
 }
