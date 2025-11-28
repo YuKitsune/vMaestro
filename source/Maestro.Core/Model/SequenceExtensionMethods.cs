df9b10263dd74a2a24c0c1b63d14cd9264d4a2b7
Eoin Motherway <25342760+YuKitsune@users.noreply.github.com>
2025-11-28 18:42:29 +1000

    fix(Insert Flight): Prevent unstable flights from being repositioned behind an inserted flight

--- source/Maestro.Core/Model/SequenceExtensionMethods.cs
+++ source/Maestro.Core/Model/SequenceExtensionMethods.cs
@@ -9,21 +9,26 @@ public static class SequenceExtensionMethods
         Flight flight,
         bool displaceStableFlights = false)
     {
-        var earliestIndex = 0;
-        if (!displaceStableFlights)
-        {
-            earliestIndex = sequence.FindLastIndex(f =>
-                f.State is not State.Unstable and not State.Stable &&
-                f.AssignedRunwayIdentifier == flight.AssignedRunwayIdentifier) + 1;
-        }
+        // TEMP: Trialing removing this logic, as unstable flights in front of inserted (frozen) flights would end up
+        // getting delayed all the way behind them.
+        // Let the scheduling move them naturally instead.
+        // TODO: We'll need to revisit this once the Scheduler has been refactored
+
+        // var earliestIndex = 0;
+        // if (!displaceStableFlights)
+        // {
+        //     earliestIndex = sequence.FindLastIndex(f =>
+        //         f.State is not State.Unstable and not State.Stable &&
+        //         f.AssignedRunwayIdentifier == flight.AssignedRunwayIdentifier) + 1;
+        // }
 
         var newIndex = sequence.FindIndex(
-            Math.Max(earliestIndex, 0),
+            // Math.Max(earliestIndex, 0),
             f => f.AssignedRunwayIdentifier == flight.AssignedRunwayIdentifier &&
                  f.LandingEstimate.IsAfter(flight.LandingEstimate));
 
         if (newIndex == -1)
-            newIndex = Math.Max(earliestIndex, sequence.Flights.Count);
+            newIndex = sequence.Flights.Count;
 
         sequence.Move(flight, newIndex, displaceStableFlights);
     }
