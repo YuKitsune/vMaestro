f62996ce042c30dfa0ca3999209a72539ce56643
Eoin Motherway <25342760+YuKitsune@users.noreply.github.com>
2025-12-13 12:55:17 +1000

    feat(UI): Fix the timeline to a specific time horizon

--- source/Maestro.Core/Configuration/ViewConfiguration.cs
+++ source/Maestro.Core/Configuration/ViewConfiguration.cs
@@ -3,8 +3,8 @@
 public class ViewConfiguration
 {
     public required string Identifier { get; init; }
+    public required int TimeHorizonMinutes { get; init; }
     public required ViewMode ViewMode { get; init; }
     public required string[] LeftLadder { get; init; }
-    
     public required string[] RightLadder { get; init; }
 }
