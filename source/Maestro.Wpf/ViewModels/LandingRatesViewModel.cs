1adf8b1a3356975fb577b93f8f2b232601598ae1
Eoin Motherway <25342760+YuKitsune@users.noreply.github.com>
2026-08-25 20:33:14 +1000

    fix: use UTC for change rates menu (#212)
    * fix: use UTC for change rates menu and other time defaults
    
    * Clean up

--- source/Maestro.Wpf/ViewModels/LandingRatesViewModel.cs
+++ source/Maestro.Wpf/ViewModels/LandingRatesViewModel.cs
@@ -56,7 +56,7 @@ public partial class LandingRatesViewModel : ObservableObject
             ? CreateRunwayConfigurationItems(currentRunwayMode, pendingLandingRatesChange)
             : CreateRunwayConfigurationItems(currentRunwayMode);
 
-        ChangeTime = pendingLandingRatesChange?.ChangeTime ?? DateTimeOffset.Now.AddMinutes(5);
+        ChangeTime = pendingLandingRatesChange?.ChangeTime ?? _clock.UtcNow().AddMinutes(5);
     }
 
     RunwayConfigurationItemViewModel[] CreateRunwayConfigurationItems(RunwayModeDto currentRunwayMode)
